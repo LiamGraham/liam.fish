44a9537fddcebaf15bfab0ed33390b2b71f7646c
Liam Graham <liam.david.graham@gmail.com>
2024-02-06 14:01:42 +1100

    Update bio and layout

--- src/App.js
+++ src/App.js
@@ -15,15 +15,13 @@ export function App() {
 
   return (
     <main>
-      <h1>Hi, I'm <span id="name" style={style}>Liam.</span></h1>
+      <h1>Hi, I'm <span id="name" className="color-change-text">Liam.</span></h1>
 
       <p className='bio'>
-        I'm a software engineer. I graduated from the University of Queensland  
+        I'm a software engineer based in Naarm / Melbourne. I graduated from the University of Queensland  
         in 2020 with a Bachelor of Engineering (Honours Class I). I'm currently working as
-        a Backend Engineer at <ColorLink href='https://www.propelhealth.ai/' mode='cycle'>Propel Health AI</ColorLink>. Here's 
+        a Principal Platform Engineer at <ColorLink href='https://www.worksafe.qld.gov.au/' mode='cycle'>WorkCover Queensland</ColorLink>. Here's 
         my <ColorLink href='https://github.com/liamgraham'>GitHub</ColorLink> and <ColorLink href='https://www.linkedin.com/in/liam-graham/'>LinkedIn</ColorLink>.
-      </p>
-      <p className='bio'>
         I also make music as <ColorLink href='https://theyalright.com'>Theyalright</ColorLink>. 
       </p>
     </main>
